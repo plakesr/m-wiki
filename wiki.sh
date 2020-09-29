26470fc9ecbd9bc7d2ca58f3a07565943e9568ae
plakesr <plakesr@gmail.com>
2020-09-29 15:51:01 +0530

    Develop (#3)
    * Update README.md
    
    * Update README.md
    
    * ansible-valut-conf
    
    * update
    
    * Delete hosts
    
    Co-authored-by: Arun Sharma <arun@Aruns-MacBook-Air.local>

--- wiki.sh
+++ wiki.sh
@@ -10,15 +10,9 @@ export mysql_ip=$(cat private_ip.txt |grep mysql | awk '{ print $5 }'|sed 's/.*=
 
 export apache_ip=$(cat private_ip.txt |grep apache | awk '{ print $7 }'|sed 's/.*=//')
 
-cat > /tmp/wiki_config.txt <<EOL
 
-Public IP : '$apache_ip'
-MySql IP : '$mysql_ip'
-DB_Name : my_wiki
-DB_USER : wiki-user
-DB_PASS: Corona@321
-EOL
 
+openssl rand -base64 6 > /tmp/matrix 
 
 cat > ./ansible/hosts <<EOL
 [web]
@@ -36,19 +30,30 @@ ansible_ssh_common_args="-o StrictHostKeyChecking=no"
 ansible_ssh_private_key_file=/tmp/id_rsa
 ansible_ssh_user=ubuntu
 EOL
-#sed -i '/\[web\]/a '$apache_ip'' ansible/hosts
-#sed -i '/\[mysql\]/a '$mysql_ip'' ansible/hosts
+
+ansible-vault encrypt --vault-password-file /tmp/matrix ansible/hosts
 
 cat > ~/.ssh/config <<EOL
 Host $mysql_ip
   ProxyCommand ssh -i /tmp/id_rsa ubuntu@'$apache_ip' nc %h %p
 EOL
 
-ansible-playbook -i ./ansible/hosts ansible/apache.yml 
 
-ansible-playbook -i ansible/hosts ansible/mysql.yml 
+ansible-playbook -i ./ansible/hosts ansible/apache.yml --vault-password-file=/tmp/matrix
 
-echo "Your Configuration details are kept in /tmp/wiki_config.txt. Once you done with LocalSetting.php. Please execute the below command from M-WIKI directory. >>> \n ansible-playbook -i ansible/hosts ansible/local_set.yml"
+ansible-playbook -i ansible/hosts ansible/mysql.yml --vault-password-file=/tmp/matrix
+
+cat > /tmp/wiki_config.txt <<EOL
+
+Public IP : '$apache_ip'
+MySql IP : '$mysql_ip'
+DB_Name : my_wiki
+DB_USER : wiki-user
+DB_PASS: "Navigate to /tmp/mysql_wiki_pass.txt File."
+
+EOL
+
+echo "Your Configuration details are kept in /tmp/wiki_config.txt. Once you done with LocalSetting.php. Please execute the below command from M-WIKI directory. >>> \n ansible-playbook -i ansible/hosts ansible/local_set.yml --vault-password-file=/tmp/matrix"
 
 
 
